b7cec380cbf0955881979b11f81d75382fb4a112
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2025-10-03 11:01:15 +0700

    feat: implement trending movies display and update search count logic

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -40,3 +40,19 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     throw error;
   }
 };
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, TABLE_NAME, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
