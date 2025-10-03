646aa6458a07a2ed544c7444b9f7be1f09f09749
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2025-10-03 10:05:17 +0700

    feat: add search count tracking with Appwrite integration

---
+++ services/appwrite.ts
@@ -0,0 +1,42 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const TABLE_NAME = "metrics";
+
+const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, TABLE_NAME, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+
+      await database.updateDocument(
+        DATABASE_ID,
+        TABLE_NAME,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, TABLE_NAME, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        title: movie.title,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
+  }
+};
